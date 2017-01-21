e5941555bf5e595ed44789d8ded4a56cf8546d83
Paul Werner <paul.werner@magicline.de>
2017-01-22 00:25:59 +0100

    init project

---
+++ src/components/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <h2>{{ name }}</h2>
+    <p>Currently Under Construction</p>
+    <img width="10%" src="https://openclipart.org/download/211907/category_state_in-development.svg" />
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      title: 'My Personal Website',
+      name: 'Paul Werner',
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2, p {
+  font-weight: normal;
+  font-family: courier;
+}
+</style>
