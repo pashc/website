b61b39f5229424f5b77796390455f0dde57618ed
Paul Werner <paulwerner@protonmail.com>
2018-04-15 16:39:20 +0200

    add mytaxi to projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -9,23 +9,29 @@
 </template>
 
 <script>
-  export default {
-    name: "Projects",
-    data ()  {
-      return {
-        title: "projects",
-        projects: [
-          {
-            name: "Magicline GmbH",
-            text: "The Magicline 2.0 is a reinvention of an award-winning complete solution " +
-            "for fitness centers based on JavaScript and Java/Spring."
-          },
-          {
-            name: "Varengold Bank AG",
-            text: "Implement a settlement software for power of attorney traders in a modern investment bank."
-          }
-        ]
-      }
+    export default {
+        name: "Projects",
+        data() {
+            return {
+                title: "projects",
+                projects: [
+                    {
+                        name: "mytaxi",
+                        text: "The world's first taxi app that provides a direct connection between passengers and drivers. " +
+                        "Expanding globally in a fast pace leads to new requirements to the system. " +
+                        "To face millions of requests the legacy solution has to move to a data-intensive architecture."
+                    },
+                    {
+                        name: "Magicline GmbH",
+                        text: "The Magicline 2.0 is a reinvention of an award-winning complete solution " +
+                        "for fitness centers based on JavaScript and Java/Spring."
+                    },
+                    {
+                        name: "Varengold Bank AG",
+                        text: "Implement a settlement software for power of attorney traders in a modern investment bank."
+                    }
+                ]
+            }
+        }
     }
-  }
 </script>
